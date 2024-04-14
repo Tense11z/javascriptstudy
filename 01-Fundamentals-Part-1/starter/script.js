d61b5397987aec32c1d7203a527742e72d079270
Tense11z <tensei.60@gmail.com>
2024-04-14 17:55:50 +0300

    Working with variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,14 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log('23');
+
+let firstName = 'Jonas';
+let first_name = 'Jake';
+let $first_name = 'Jessica';
+let PI = 3.1415
+console.log(firstName);
+console.log(first_name);
+console.log($first_name);
+console.log(PI);
