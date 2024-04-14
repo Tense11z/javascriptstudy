2d50578f5b025c5c05553286dc91b0c68bf087c3
Tense11z <tensei.60@gmail.com>
2024-04-14 18:31:41 +0300

    Commenting and working with data types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-let js = 'amazing';
+/*let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
 console.log('Jonas');
@@ -12,3 +12,24 @@ console.log(firstName);
 console.log(first_name);
 console.log($first_name);
 console.log(PI);
+*/
+let javascriptIsFun = true; // declearing boolean variable
+console.log(javascriptIsFun); // printing boolean variable
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas')
+
+javascriptIsFun = 'YES!';
+
+// console.log(javascriptIsFun)
+console.log(typeof javascriptIsFun);
+
+let year; // declearing undefined variable
+console.log(year);
+console.log(typeof year);
+
+year = 1999;
+console.log(year);
+console.log(typeof year);
