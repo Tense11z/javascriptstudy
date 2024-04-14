5204fb1e34e648b471df261dccd91d1bc171108c
Tense11z <tensei.60@gmail.com>
2024-04-14 19:12:26 +0300

    math / boolean operators and operations

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -13,27 +13,52 @@ console.log(first_name);
 console.log($first_name);
 console.log(PI);
 */
-// let javascriptIsFun = true; // declearing boolean variable
-// console.log(javascriptIsFun); // printing boolean variable
+/*let javascriptIsFun = true; // declearing boolean variable
+console.log(javascriptIsFun); // printing boolean variable
 
-// // console.log(typeof true);
-// console.log(typeof javascriptIsFun);
-// // console.log(typeof 23);
-// // console.log(typeof 'Jonas')
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas')
 
-// javascriptIsFun = 'YES!';
+javascriptIsFun = 'YES!';
 
-// // console.log(javascriptIsFun)
-// console.log(typeof javascriptIsFun);
+// console.log(javascriptIsFun)
+console.log(typeof javascriptIsFun);
 
-// let year; // declearing undefined variable
-// console.log(year);
-// console.log(typeof year);
+let year; // declearing undefined variable
+console.log(year);
+console.log(typeof year);
 
-// year = 1999;
-// console.log(year);
-// console.log(typeof year);
+year = 1999;
+console.log(year);
+console.log(typeof year); */
 
-let age = 24; // declaring mutable variable
+/*let age = 24; // declaring mutable variable
 const birthYear = 1999; // declaring constant variable; const cannot be empty
-var job = 'programmer'; // legacy approach tp declare mutable variable
+var job = 'programmer'; // legacy approach tp declare mutable variable*/
+
+// math operators and operations
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;
+x += 10; // incrementing ; the same is possible using different opperators
+x++; // incrementing on 1  
+console.log(x);
+
+// comparison operators and  operations
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=, ===
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > now - 2018);
