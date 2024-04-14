94064397602c634c59b712cd76bed0c987cd0688
Tense11z <tensei.60@gmail.com>
2024-04-14 18:40:16 +0300

    investigating how to declare variables using let, const, var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -13,23 +13,27 @@ console.log(first_name);
 console.log($first_name);
 console.log(PI);
 */
-let javascriptIsFun = true; // declearing boolean variable
-console.log(javascriptIsFun); // printing boolean variable
+// let javascriptIsFun = true; // declearing boolean variable
+// console.log(javascriptIsFun); // printing boolean variable
 
-// console.log(typeof true);
-console.log(typeof javascriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof 'Jonas')
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jonas')
 
-javascriptIsFun = 'YES!';
+// javascriptIsFun = 'YES!';
 
-// console.log(javascriptIsFun)
-console.log(typeof javascriptIsFun);
+// // console.log(javascriptIsFun)
+// console.log(typeof javascriptIsFun);
 
-let year; // declearing undefined variable
-console.log(year);
-console.log(typeof year);
+// let year; // declearing undefined variable
+// console.log(year);
+// console.log(typeof year);
 
-year = 1999;
-console.log(year);
-console.log(typeof year);
+// year = 1999;
+// console.log(year);
+// console.log(typeof year);
+
+let age = 24; // declaring mutable variable
+const birthYear = 1999; // declaring constant variable; const cannot be empty
+var job = 'programmer'; // legacy approach tp declare mutable variable
