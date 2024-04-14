d9eabc194ea3477c9b60be145a5f64e1f4b3005c
Tense11z <tensei.60@gmail.com>
2024-04-15 00:02:29 +0300

    Code challenge#2 is resolved

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,7 +108,7 @@ console.log(`Multiple
 lines 
 whithout \\n `)*/
 
-// if statements
+/*// if statements
 
 const age = 18, ageS = 15;
 const isOldEnough = age >= 18;
@@ -132,4 +132,22 @@ if (birthYear <= 2000) {
     century = 21;
 }
 
-console.log(century)
+console.log(century)*/
+
+// code challenge#2
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`)
+} else {
+    console.log(`John's BMI ${BMIJohn} is higher than Mark's ${BMIMark}!`)
+}
