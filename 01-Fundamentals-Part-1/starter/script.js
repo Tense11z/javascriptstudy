18329ea8986575fad9accefd8a7ca3614ca61408
Tense11z <tensei.60@gmail.com>
2024-04-14 22:48:42 +0300

    operator precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -39,7 +39,7 @@ const birthYear = 1999; // declaring constant variable; const cannot be empty
 var job = 'programmer'; // legacy approach tp declare mutable variable*/
 
 // math operators and operations
-const now = 2037;
+/*const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
@@ -61,4 +61,16 @@ console.log(ageJonas > ageSarah); // >, <, >=, <=, ===
 console.log(ageSarah >= 18);
 
 const isFullAge = ageSarah >= 18;
-console.log(now - 1991 > now - 2018);
+console.log(now - 1991 > now - 2018);*/
+
+// operator precedence 
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10; x = 10;
+console.log(x, y);
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+
+const averageAge = (ageJonas + ageSarah) / 2
+console.log(ageJonas, ageSarah, averageAge)
