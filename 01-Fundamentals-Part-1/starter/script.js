3b459dba39b302a1741714b7631cda43498741d3
Tense11z <tensei.60@gmail.com>
2024-04-16 21:06:02 +0300

    adding codingheroes "switch" challenge

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -259,3 +259,41 @@ switch (day) {
     default:
         console.log('Not a Valid day!');
 }
+
+// =======================================================
+// implementation of the switch statement above, using IFs
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+} else {
+    console.log('Not a Valid day!');
+}
+
+// codingheroes.io challange
+
+const language = 'ukrainian';
+switch (language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log('MOST number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place');
+        break;
+    case 'hindi':
+        console.log('Number 4');
+        break;
+    case 'arabic':
+        console.log('5th most spoken language');
+        break;
+    default:
+        console.log('Great language too :D');
+}
