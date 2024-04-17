8a18d97e6ee046fd99c48e67739ee3e94bf239c2
Tense11z <tensei.60@gmail.com>
2024-04-17 20:34:33 +0300

    strict mode

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,10 @@
-'use strict';
+// ============================
+'use strict'; // helps to trow errors 
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;// will throw 'Uncaught ReferenceError: hasDriverLicense is not defined'
+if (hasDriversLicense) console.log('I can drive');
+
+const private = 'Audio' // will 'throw Unexpected strict mode reserved word'
