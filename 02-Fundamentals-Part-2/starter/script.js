3a5e2b141c87bff73064dd89f9b4ae49b6e3eb16
Tense11z <tensei.60@gmail.com>
2024-04-17 21:53:56 +0300

    added codingheroes declarationvsexpression challenge

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -32,7 +32,7 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 */
 
 // codingheroes functions challenge
-
+/*
 function describeCountry(country, population, capitalCity) {
     console.log(country, population, capitalCity);
     const Description = `${country} has ${population} million people and its capital city is ${capitalCity}. \n`;
@@ -43,3 +43,43 @@ const countryDescription1 = describeCountry('Ukraine', 38, 'Kyiv');
 const countryDescription2 = describeCountry('France', 67, 'Paris');
 const countryDescription3 = describeCountry('Republic of Korea', 51, 'Seoul');
 console.log(countryDescription1, countryDescription2, countryDescription3)
+*/
+
+//============================
+
+function calcAge1(birthYear) { // function declaration; can be called before declaration
+    return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+console.log(age1);
+
+const calcAge2 = function (birthYear) { //function expression; cannot be called before declaration
+    return 2037 - birthYear;
+}
+
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+// codingheroes Function Declarations vs. Expressions challenge
+
+function percentageOfWorld1(population) { //declaration
+    const worldPopulation = 7900;
+    return population / worldPopulation * 100;
+}
+
+const ukrainianPopulationPercentage1 = percentageOfWorld1(33);
+const francesPopulationPercentage1 = percentageOfWorld1(67);
+const republicOfKoreaPopulationPercentage1 = percentageOfWorld1(51);
+
+console.log(ukrainianPopulationPercentage1, francesPopulationPercentage1, republicOfKoreaPopulationPercentage1);
+
+const percentageOfWorld2 = function (population) { //declaration
+    const worldPopulation = 7900;
+    return population / worldPopulation * 100;
+}
+const ukrainianPopulationPercentage2 = percentageOfWorld2(33);
+const francesPopulationPercentage2 = percentageOfWorld2(67);
+const republicOfKoreaPopulationPercentage2 = percentageOfWorld2(51);
+console.log(ukrainianPopulationPercentage2, francesPopulationPercentage2, republicOfKoreaPopulationPercentage2);
