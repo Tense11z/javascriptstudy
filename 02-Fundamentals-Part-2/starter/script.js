a40684a32ad41ad31bc1b08d971b8b3b0710e4ba
Tense11z <tensei.60@gmail.com>
2024-04-17 20:58:25 +0300

    intro to functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 // ============================
-'use strict'; // helps to trow errors 
+/*'use strict'; // helps to trow errors 
 
 let hasDriversLicense = false;
 const passTest = true;
@@ -8,3 +8,24 @@ if (passTest) hasDriverLicense = true;// will throw 'Uncaught ReferenceError: ha
 if (hasDriversLicense) console.log('I can drive');
 
 const private = 'Audio' // will 'throw Unexpected strict mode reserved word'
+*/
+
+// ============================
+
+function logger() {
+    console.log('My name is Jonas');
+}
+
+logger(); // calling / running / invoking function
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
