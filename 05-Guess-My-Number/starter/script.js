b04e08e549f4fac40aa87d4620a63924c091da45
Tense11z <tensei.60@gmail.com>
2024-04-26 01:07:15 +0300

    added win and lose conditions

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,3 +11,32 @@ document.querySelector('.guess').value = 23;  // set content
 
 console.log(document.querySelector('.guess').value);
 */
+
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+let score = 20;
+
+
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess);
+    if(score){
+        if(!guess) {
+            document.querySelector('.message').textContent = '⛔ No number!';
+        } else if (secretNumber === guess){
+            document.querySelector('.message').textContent='🎉 Correct Number!';
+        } else if (secretNumber < guess){
+            score -=1;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent='📈 Too High!';
+        } else if (secretNumber > guess){
+            score -=1;
+            document.querySelector('.score').textContent = score;
+            document.querySelector('.message').textContent='📉 Too Low!';
+        }
+    } else {
+        document.querySelector('.message').textContent='☠ You Lost!';
+    }
+})
