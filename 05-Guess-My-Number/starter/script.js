3eb809c5a2ef7affbd4d24b4cdb0f4d99332d2f6
Tense11z <tensei.60@gmail.com>
2024-04-26 00:18:45 +0300

    introduction to the querySelector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+ /*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent='🎉 Correct Number!'
+
+document.querySelector('.number').textContent =13; // get content
+document.querySelector('.score').textContent =10;
+
+
+document.querySelector('.guess').value = 23;  // set content
+
+console.log(document.querySelector('.guess').value);
+*/
