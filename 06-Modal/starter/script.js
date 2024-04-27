8d6bfaa64ca390143a47881849c81a704a1f9e2c
Tense11z <tensei.60@gmail.com>
2024-04-27 17:33:20 +0300

    refactored code

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,26 +4,24 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for(let i = 0; i < btnsShowModal.length; i+=1) {
-    btnsShowModal[i].addEventListener('click', function(){
-        modal.classList.remove('hidden');
-        overlay.classList.remove('hidden');
-    });
+
+const openModal = function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 };
 
-btnCloseModal.addEventListener('click', function(){
+const closeModal = function() {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-});
+}
 
-overlay.addEventListener('click', function() {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-});
+for(let i = 0; i < btnsShowModal.length; i+=1) {
+    btnsShowModal[i].addEventListener('click', openModal)
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown',function(e) {
-    if (e.key == 'Escape') {
-        modal.classList.add('hidden');
-        overlay.classList.add('hidden');
-    };
+    if (e.key == 'Escape') closeModal();
 });
