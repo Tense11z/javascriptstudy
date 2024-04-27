4fc0a0454a754b7dbc50cbc4461fedfa748b1d69
Tense11z <tensei.60@gmail.com>
2024-04-27 17:08:10 +0300

    added event listener to show/close buttons, also added keydown event so modal can be closed on "Esc" button

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsShowModal = document.querySelectorAll('.show-modal');
+
+for(let i = 0; i < btnsShowModal.length; i+=1) {
+    btnsShowModal[i].addEventListener('click', function(){
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+};
+
+btnCloseModal.addEventListener('click', function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown',function(e) {
+    if (e.key == 'Escape') {
+        modal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    };
+});
