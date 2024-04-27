1fd1e401b4e07b86ae1da8cb0e9bfbc6a87749da
Tense11z <tensei.60@gmail.com>
2024-04-27 18:18:40 +0300

    added hidden class to style.css & starting conditions to the script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+// selecting elements 
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+//starting conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
